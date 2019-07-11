afe032ef427e9cd6f0a70baf8957c922540e4079
Jacek Ratajewski <51850397+JacekRatajewski@users.noreply.github.com>
2019-07-11 13:15:07 +0200

    add core project

---
+++ GenericDispatcher.Core/GDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GenericDispatcher.Core
+{
+    public class GDispatcher<T>
+    {
+        public static async Task<T> Dispatch(string path)
+        {
+            using (var file = new StreamReader(path))
+            {
+                string json = await file.ReadToEndAsync();
+                var data = JsonConvert.DeserializeObject<T>(json);
+                return data;
+            }
+        }
+
+        public static void Adopt(string path, T data)
+        {
+            using (StreamWriter file = (!File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(file, data);
+            }
+        }
+    }
+}
