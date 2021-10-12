757fbf0019ce0cdf65417da0d569165ac57efb13
Jacek Ratajewski <51850397+JacekRatajewski@users.noreply.github.com>
2021-10-12 10:30:23 +0200

    simplify encoding fix

--- GenericDispatcher.Core/GDispatcher.cs
+++ GenericDispatcher.Core/GDispatcher.cs
@@ -5,7 +5,7 @@ namespace GenericDispatcher.Core
 {
     public class GDispatcher<T>
     {
-        public static async Task<T> Dispatch(string path, Encoding encoding = null) => JsonConvert.DeserializeObject<T>(encoding is null ? await new StreamReader(path).ReadToEndAsync() : encoding.GetString(Encoding.Default.GetBytes(await new StreamReader(path).ReadToEndAsync())));
+        public static async Task<T> Dispatch(string path, Encoding encoding = null) => JsonConvert.DeserializeObject<T>(await new StreamReader(path, encoding, true).ReadToEndAsync());
 
         public static void Adopt(string path, T data)
         {
